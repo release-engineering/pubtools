3acbeaef322a752e124fd0d4156d7a18b95bb1d6
Rohan McGovern <rmcgover@redhat.com>
2021-08-04 14:39:51 +1000

    hooks: add eager loading of entry points to ensure initialization
    The recommended approach to registering hookimpls and hookspecs is to
    register them when your module is imported.
    
    Problem: there can be modules which want to declare hooks but
    wouldn't be imported during the natural course of events while running a
    task. Hence, hooks could be missing.
    
    The mkhooks script in this repo already ran into this problem and it
    attempted to solve it by eagerly importing all console_scripts. That
    works for projects like pubtools-quay, but is insufficient in general
    because there may be "hook-only" libraries which have no
    console_scripts.
    
    For example, consider a hypothetical pubtools-umb library with desired
    behavior: if it's installed, and certain env vars are set, it will
    attach to various hooks and send messages on a message bus when those
    are activated.
    
    Such a project has no need of any console_script, and we also don't want
    to require each project to "import pubtools.umb" just to activate it.
    So, there is no obvious way to arrange for the relevant module to be
    imported.
    
    This commit provides a solution: a hook-only library such as
    pubtools-umb can declare an entry point in pubtools.hooks, and
    task_context will ensure any such entry points are fully resolved before
    proceeding.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_requirements():
 
 setup(
     name="pubtools",
-    version="1.0.0",
+    version="1.1.0",
     packages=find_packages(exclude=["tests"]),
     url="https://github.com/release-engineering/pubtools",
     license="GNU General Public License",
---
+++ tests/hooks/test_entry_points.py
@@ -0,0 +1,52 @@
+import sys
+
+from pkg_resources import get_distribution, EntryPoint
+
+from pubtools.pluggy import task_context
+
+
+def test_task_context_loads_entry_points(monkeypatch):
+    """task_context eagerly resolves console_scripts and pubtools.hooks entry points."""
+
+    # Need some valid distribution to use here; don't care which one.
+    # Just pick something we know is installed.
+    dist = get_distribution("pytest")
+
+    entry_map = dist.get_entry_map()
+
+    # This group already exists in pytest.
+    scripts = entry_map["console_scripts"]
+
+    # This is a new group we'll force onto the map.
+    hooks = {}
+    monkeypatch.setitem(entry_map, "pubtools.hooks", hooks)
+
+    # Set up some console scripts entry points. We'll make them point at existing
+    # modules so that an import will succeed.
+    #
+    # This one is pubtools.* , so it should be imported
+    monkeypatch.setitem(
+        scripts, "some-script", EntryPoint("some-script", "pubtools.pluggy")
+    )
+
+    # This should be ignored
+    monkeypatch.setitem(
+        scripts, "other-script", EntryPoint("other-script", "something.non.existent")
+    )
+
+    # Set up a hook entry point.
+    monkeypatch.setitem(hooks, "anything", EntryPoint("anything", "pytest"))
+
+    # Effectively "un-import" these modules so we can observe that task_context
+    # has the side-effect of importing them.
+    assert "pubtools.pluggy" in sys.modules
+    assert "pytest" in sys.modules
+    del sys.modules["pubtools.pluggy"]
+    del sys.modules["pytest"]
+
+    with task_context():
+        # As soon as we've entered the task context, the modules referenced from
+        # those entry points should be imported, ensuring we can now use hooks with
+        # everything registered
+        assert "pubtools.pluggy" in sys.modules
+        assert "pytest" in sys.modules
