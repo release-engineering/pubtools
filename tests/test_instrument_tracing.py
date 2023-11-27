261be6cec9171c0b4d2782d4dd9edd145376c8b5
Zhiming Xiong <zxiong@redhat.com>
2023-11-28 00:17:50 +0800

    Improve instrument tracing tests

--- tests/test_instrument_tracing.py
+++ tests/test_instrument_tracing.py
@@ -1,47 +1,87 @@
 import os
+from collections import deque
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from unittest import mock
-from unittest.mock import Mock
 
-import pytest
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import \
-    OTLPSpanExporter
+from opentelemetry.sdk.trace.export import SpanExportResult
 
 from pubtools.tracing import TracingWrapper, instrument_func
 
 
-@mock.patch.dict(
-    os.environ, {"PUB_OTEL_TRACING": "true", "traceparent": "00-xxxx-xxx-01"}
-)
-def test_instrument_func_enabled():
-    mock_export = Mock()
-    OTLPSpanExporter.export = mock_export
+def test_instrument_func_multiple_threads():
+    trace_id = "cefb2b8db35d5f3c0dfdf79d5aab1451"
+    span_id = "1f2bb7927f140744"
+    os.environ["PUB_OTEL_TRACING"] = "true"
+    os.environ["traceparent"] = f"00-{trace_id}-{span_id}-01"
+    os.environ["OTEL_SERVICE_NAME"] = "local-test"
+    os.environ["OTEL_EXPORTER_OTLP_ENDPOINT"] = "http://mock"
 
-    @instrument_func(args_to_attr=True)
-    def func_normal():
+    # save spans
+    queue = deque()
+
+    # mock OTLPSpanExporter.export()
+    def mock_export(spans):
+        for span in spans:
+            queue.append(span)
+        return SpanExportResult.SUCCESS
+
+    otlp_exporter = TracingWrapper().processor.span_exporter
+    otlp_exporter.export = mock_export
+
+    @instrument_func(span_name="sub_thread_span")
+    def sub_thread():
         return 1
 
-    @instrument_func()
-    def func_with_exception():
-        raise Exception()
+    @instrument_func(span_name="main_thread_span", args_to_attr=True)
+    def main_thread(param1, param2):
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            future_res = [executor.submit(sub_thread) for i in range(1, 2)]
+            as_completed(future_res)
 
-    assert TracingWrapper()
-    assert func_normal() == 1
-    with pytest.raises(Exception):
-        func_with_exception()
+    main_thread("p1", param2="p2")
 
     TracingWrapper.processor.force_flush()
-    mock_export.assert_called()
+
+    out_spans = list(queue)
+    # There are two spans which named main_thread_span and sub_thread_span respectively
+    assert len(out_spans) == 2
+
+    main_thread_span = None
+    sub_thread_span = None
+    for span in out_spans:
+        if span.name == "main_thread_span":
+            main_thread_span = span
+        if span.name == "sub_thread_span":
+            sub_thread_span = span
+
+    assert main_thread_span
+    assert sub_thread_span
+
+    # Both are in the same trace
+    assert sub_thread_span.context.trace_id == main_thread_span.context.trace_id
+
+    # The span's parent in sub-thread is the span in main thread.
+    assert sub_thread_span.parent.span_id == main_thread_span.context.span_id
+
+    # The parent of the span in main thread
+    assert str(hex(main_thread_span.parent.span_id)) == f"0x{span_id}"
+
+    # Assert function parameters in span attributes
+    assert (
+        "args" in main_thread_span.attributes
+        and main_thread_span.attributes["args"] == "p1"
+    )
+    assert (
+        "kwargs" in main_thread_span.attributes
+        and main_thread_span.attributes["kwargs"] == "param2=p2"
+    )
 
 
 @mock.patch.dict(os.environ, {"PUB_OTEL_TRACING": "false"})
 def test_instrument_func_disabled():
-    mock_export = Mock()
-    OTLPSpanExporter.export = mock_export
-
     @instrument_func()
     def func_normal():
         return 1
 
     assert not TracingWrapper()
     assert func_normal() == 1
-    mock_export.assert_not_called()
